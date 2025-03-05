e05c0f2b4689c12a220ade9f158c0cd64c6f12f0
Arturs <arturshugo4@gmail.com>
2025-03-05 09:43:46 +0200

    first push

---
+++ laravel-posts-api-1/app/Http/Controllers/PostController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+
+class PostController extends Controller implements HasMiddleware
+{
+
+    public static function middleware() {
+        return [
+            new Middleware('auth:sanctum', except: ['index', 'show'])
+        ];
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Post::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+            'image_path' => 'mimes:jpeg,png,jpg,gif,svg'
+        ]);
+
+        $post = $request->user()->posts()->create($fields);
+
+        return ['post' => $post];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return ['post' => $post];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+            'image_path' => 'mimes:jpeg,png,jpg,gif,svg'
+        ]);
+
+        $post->update($fields);
+
+        return ['post' => $post];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return ['message' => 'The post was deleted!'];
+    }
+}
