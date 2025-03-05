fa41e4658417bac41f7b7ee1bf3dc865fc5d2697
Arturs <arturshugo4@gmail.com>
2025-03-05 10:56:35 +0200

    started authController

--- laravel-api-noauth-broken/routes/api.php
+++ laravel-api-noauth-broken/routes/api.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
 Route::apiResource('posts', PostController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::get('/', function() {
     return 'API';
