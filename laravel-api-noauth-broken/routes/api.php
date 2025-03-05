bc98c453853748daf147c9a0462118e7e9c70238
Arturs <arturshugo4@gmail.com>
2025-03-05 09:53:44 +0200

    gave root a return

--- laravel-api-noauth-broken/routes/api.php
+++ laravel-api-noauth-broken/routes/api.php
@@ -1,10 +1,14 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::apiResource('posts', PostController::class);
+
+Route::get('/', function() {
+    return 'API';
+});
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
